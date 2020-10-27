e6b35440f0cff4e1e73b32990e56827e88a43272
Gustavo Máximo <gfmaximo97@gmail.com>
2020-10-27 16:48:09 -0300

    refactor: Callback approach to promises.

--- response-handler.js
+++ response-handler.js
@@ -1,15 +1,19 @@
 import { fileURLToPath } from 'url';
 import { join, dirname } from 'path';
+import fs from 'fs/promises';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const resHandler = (req, res, next) => {
-    // fs.readFile('my-page.html', 'utf8', (err, data) => {
-    //     res.send(data);
-    // });
+const resHandler = async (req, res, next) => {
+    try {
+        const data = await fs.readFile('my-page.html', 'utf8');
+        res.send(data);
+    } catch (err) {
+        console.log(err);
+    }
 
-    res.sendFile(join(__dirname, 'my-page.html'));
+    // res.sendFile(join(__dirname, 'my-page.html'));
 };
 
 export default resHandler;
